c7e96068a56da444e78cc7739a902da8e268dc63
Boris <bpozdnyakov@gmail.com>
2021-07-19 17:44:09 +0300

    fix (#6)

--- apps/limiter/src/lim_turnover_processor.erl
+++ apps/limiter/src/lim_turnover_processor.erl
@@ -52,7 +52,7 @@ get_limit(LimitID, Config, LimitContext) ->
         LimitRangeID = construct_range_id(LimitID, Timestamp, Config, LimitContext),
         LimitRange = unwrap(limit, lim_range_machine:get(LimitRangeID, LimitContext)),
         TimeRange = lim_config_machine:calculate_time_range(Timestamp, Config),
-        #{own_amount := Amount} =
+        #{max_available_amount := Amount} =
             unwrap(lim_range_machine:get_range_balance(TimeRange, LimitRange, LimitContext)),
         #limiter_Limit{
             id = LimitRangeID,
