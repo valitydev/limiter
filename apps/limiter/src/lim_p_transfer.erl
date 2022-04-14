7077381f969ba848fcdd10cfaf2cf68d2954f294
Andrew Mayorov <encube.ul@gmail.com>
2022-04-14 20:15:19 +0300

    Fix compilation warnings

--- apps/limiter/src/lim_p_transfer.erl
+++ apps/limiter/src/lim_p_transfer.erl
@@ -56,13 +56,13 @@ reverse_postings(Postings) ->
 
 -spec assert_partial_posting_amount([posting()], [posting()]) -> ok | {error, forbidden_operation_amount_error()}.
 assert_partial_posting_amount(
-    [#accounter_Posting{amount = Partial, currency_sym_code = Currency} | _Rest],
-    [#accounter_Posting{amount = Full, currency_sym_code = Currency} | _Rest]
+    [#accounter_Posting{amount = Partial, currency_sym_code = Currency} | _],
+    [#accounter_Posting{amount = Full, currency_sym_code = Currency} | _]
 ) ->
     compare_amount(Partial, Full, Currency);
 assert_partial_posting_amount(
-    [#accounter_Posting{amount = Partial, currency_sym_code = PartialCurrency} | _Rest],
-    [#accounter_Posting{amount = Full, currency_sym_code = FullCurrency} | _Rest]
+    [#accounter_Posting{amount = Partial, currency_sym_code = PartialCurrency} | _],
+    [#accounter_Posting{amount = Full, currency_sym_code = FullCurrency} | _]
 ) ->
     erlang:error({invalid_partial_cash, {Partial, PartialCurrency}, {Full, FullCurrency}}).
 
