ffaa6f0c3d9ee0c4b0c375ab5ca13120465d0c58
Aleksey Kashapov <nanodirijabl@gmail.com>
2025-10-09 11:32:44 +0300

    Retires codec modules and moves sole functions into user-modules

--- apps/limiter/src/lim_config_machine.erl
+++ apps/limiter/src/lim_config_machine.erl
@@ -2,6 +2,7 @@
 
 -include_lib("limiter_proto/include/limproto_limiter_thrift.hrl").
 -include_lib("damsel/include/dmsl_domain_thrift.hrl").
+-include_lib("damsel/include/dmsl_limiter_config_thrift.hrl").
 -include_lib("damsel/include/dmsl_domain_conf_v2_thrift.hrl").
 
 %% Accessors
@@ -212,7 +213,8 @@ get_config(ID, Version, #{woody_context := WoodyContext}) ->
     try
         #domain_conf_v2_VersionedObject{object = {limit_config, ConfigObject}} =
             dmt_client:checkout_object(Version, LimitConfigRef, #{woody_context => WoodyContext}),
-        {ok, lim_config_codec:unmarshal('LimitConfigObject', ConfigObject)}
+        {ok, unmarshal_limit_config(ConfigObject)}
+        %% {ok, lim_config_codec:unmarshal('LimitConfigObject', ConfigObject)}
     catch
         throw:#domain_conf_v2_ObjectNotFound{} ->
             {error, notfound}
@@ -230,8 +232,8 @@ calculate_shard_id(Timestamp, Config) ->
     end.
 
 calculate_calendar_shard_id(Range, Timestamp, StartedAt, ShardSize) ->
-    StartDatetime = lim_range_codec:parse_timestamp(StartedAt),
-    CurrentDatetime = lim_range_codec:parse_timestamp(Timestamp),
+    StartDatetime = parse_timestamp(StartedAt),
+    CurrentDatetime = parse_timestamp(Timestamp),
     Units = calculate_time_units(Range, CurrentDatetime, StartDatetime),
     SignPrefix = mk_sign_prefix(Units),
     RangePrefix = mk_unit_prefix(Range),
@@ -476,6 +478,125 @@ get_generic_payment_tool_resource(ContextType, LimitContext) ->
             Error
     end.
 
+parse_timestamp(Bin) ->
+    try
+        MicroSeconds = genlib_rfc3339:parse(Bin, microsecond),
+        case genlib_rfc3339:is_utc(Bin) of
+            false ->
+                erlang:error({bad_timestamp, not_utc}, [Bin]);
+            true ->
+                calendar:system_time_to_universal_time(MicroSeconds, microsecond)
+        end
+    catch
+        error:Error:St ->
+            erlang:raise(error, {bad_timestamp, Bin, Error}, St)
+    end.
+
+unmarshal_limit_config(#domain_LimitConfigObject{
+    ref = #domain_LimitConfigRef{id = ID},
+    data = #limiter_config_LimitConfig{
+        processor_type = ProcessorType,
+        started_at = StartedAt,
+        shard_size = ShardSize,
+        time_range_type = TimeRangeType,
+        context_type = ContextType,
+        type = Type,
+        scopes = Scopes,
+        description = Description,
+        op_behaviour = OpBehaviour,
+        currency_conversion = CurrencyConversion
+    }
+}) ->
+    genlib_map:compact(#{
+        id => ID,
+        processor_type => ProcessorType,
+        started_at => StartedAt,
+        shard_size => ShardSize,
+        time_range_type => unmarshal_time_range_type(TimeRangeType),
+        context_type => unmarshal_context_type(ContextType),
+        type => maybe_apply(Type, fun unmarshal_type/1),
+        scope => maybe_apply(Scopes, fun unmarshal_scope/1),
+        description => Description,
+        op_behaviour => maybe_apply(OpBehaviour, fun unmarshal_op_behaviour/1),
+        currency_conversion => CurrencyConversion =/= undefined
+    }).
+
+unmarshal_time_range_type({calendar, CalendarType}) ->
+    {calendar, unmarshal_calendar_time_range_type(CalendarType)};
+unmarshal_time_range_type({interval, #limiter_config_TimeRangeTypeInterval{amount = Amount}}) ->
+    {interval, Amount}.
+
+unmarshal_calendar_time_range_type({day, _}) ->
+    day;
+unmarshal_calendar_time_range_type({week, _}) ->
+    week;
+unmarshal_calendar_time_range_type({month, _}) ->
+    month;
+unmarshal_calendar_time_range_type({year, _}) ->
+    year.
+
+unmarshal_context_type({payment_processing, #limiter_config_LimitContextTypePaymentProcessing{}}) ->
+    payment_processing;
+unmarshal_context_type({withdrawal_processing, #limiter_config_LimitContextTypeWithdrawalProcessing{}}) ->
+    withdrawal_processing.
+
+unmarshal_type({turnover, #limiter_config_LimitTypeTurnover{metric = Metric}}) ->
+    {turnover, maybe_apply(Metric, fun unmarshal_turnover_metric/1, number)}.
+
+unmarshal_turnover_metric({number, _}) ->
+    number;
+unmarshal_turnover_metric({amount, #limiter_config_LimitTurnoverAmount{currency = Currency}}) ->
+    {amount, Currency}.
+
+unmarshal_scope({single, Type}) ->
+    ordsets:from_list([unmarshal_scope_type(Type)]);
+unmarshal_scope({multi, Types}) ->
+    ordsets:from_list(lists:map(fun unmarshal_scope_type/1, ordsets:to_list(Types)));
+unmarshal_scope(Types) when is_list(Types) ->
+    ordsets:from_list(lists:map(fun unmarshal_scope_type/1, ordsets:to_list(Types))).
+
+unmarshal_scope_type({party, _}) ->
+    party;
+unmarshal_scope_type({shop, _}) ->
+    shop;
+unmarshal_scope_type({wallet, _}) ->
+    wallet;
+unmarshal_scope_type({payment_tool, _}) ->
+    payment_tool;
+unmarshal_scope_type({provider, _}) ->
+    provider;
+unmarshal_scope_type({terminal, _}) ->
+    terminal;
+unmarshal_scope_type({payer_contact_email, _}) ->
+    payer_contact_email;
+unmarshal_scope_type({destination_field, #limiter_config_LimitScopeDestinationFieldDetails{field_path = FieldPath}}) ->
+    %% Domain config variant clause
+    {destination_field, FieldPath};
+unmarshal_scope_type({sender, _}) ->
+    sender;
+unmarshal_scope_type({receiver, _}) ->
+    receiver.
+
+unmarshal_op_behaviour(#limiter_config_OperationLimitBehaviour{invoice_payment_refund = Refund}) ->
+    genlib_map:compact(#{
+        invoice_payment_refund => maybe_apply(Refund, fun unmarshal_behaviour/1)
+    }).
+
+unmarshal_behaviour({subtraction, #limiter_config_Subtraction{}}) ->
+    subtraction;
+unmarshal_behaviour({addition, #limiter_config_Addition{}}) ->
+    addition.
+
+maybe_apply(undefined, _) ->
+    undefined;
+maybe_apply(Value, Fun) ->
+    Fun(Value).
+
+maybe_apply(undefined, _, Default) ->
+    Default;
+maybe_apply(Value, Fun, _Default) ->
+    Fun(Value).
+
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
@@ -487,8 +608,44 @@ get_generic_payment_tool_resource(ContextType, LimitContext) ->
 
 -spec test() -> _.
 
--spec check_sign_prefix_test() -> _.
+-spec unmarshal_config_object_test() -> _.
+unmarshal_config_object_test() ->
+    Config = #{
+        id => <<"id">>,
+        processor_type => <<"type">>,
+        started_at => <<"2000-01-01T00:00:00Z">>,
+        shard_size => 7,
+        time_range_type => {calendar, day},
+        context_type => payment_processing,
+        type => {turnover, number},
+        scope => ordsets:from_list([party, shop, {destination_field, [<<"path">>, <<"to">>, <<"field">>]}]),
+        description => <<"description">>,
+        currency_conversion => true
+    },
+    Object = #domain_LimitConfigObject{
+        ref = #domain_LimitConfigRef{id = <<"id">>},
+        data = #limiter_config_LimitConfig{
+            processor_type = <<"type">>,
+            started_at = <<"2000-01-01T00:00:00Z">>,
+            shard_size = 7,
+            time_range_type = {calendar, {day, #limiter_config_TimeRangeTypeCalendarDay{}}},
+            context_type = {payment_processing, #limiter_config_LimitContextTypePaymentProcessing{}},
+            type =
+                {turnover, #limiter_config_LimitTypeTurnover{metric = {number, #limiter_config_LimitTurnoverNumber{}}}},
+            scopes = ordsets:from_list([
+                {'party', #limiter_config_LimitScopeEmptyDetails{}},
+                {'shop', #limiter_config_LimitScopeEmptyDetails{}},
+                {'destination_field', #limiter_config_LimitScopeDestinationFieldDetails{
+                    field_path = [<<"path">>, <<"to">>, <<"field">>]
+                }}
+            ]),
+            description = <<"description">>,
+            currency_conversion = #limiter_config_CurrencyConversion{}
+        }
+    },
+    ?assertEqual(Config, unmarshal_limit_config(Object)).
 
+-spec check_sign_prefix_test() -> _.
 check_sign_prefix_test() ->
     ?assertEqual(<<"past">>, mk_sign_prefix(-10)),
     ?assertEqual(<<"future">>, mk_sign_prefix(0)),
